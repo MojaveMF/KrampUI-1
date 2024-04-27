48f5585906510dc852d9beb576f4ef233c25a360
mojavemf <funnyfailiers@gmail.com>
2024-04-27 23:53:49 +0100

    Type safety for setSetting

--- src/managers/SettingsManager.ts
+++ src/managers/SettingsManager.ts
@@ -3,14 +3,14 @@ import { FileSystemService } from "../services/FilesystemService";
 import WindowManager from "./WindowManager";
 
 export type Settings = {
-    autoInject: boolean,
-    topMost: boolean,
-    homeToggleEnabled: boolean,
-    editorFontSize: number,
-    injectionDelay: number,
-    autoUpdate: boolean,
-    [key: string]: any
-}
+    autoInject: boolean;
+    topMost: boolean;
+    homeToggleEnabled: boolean;
+    editorFontSize: number;
+    injectionDelay: number;
+    autoUpdate: boolean;
+    [key: string]: any;
+};
 
 export default class SettingsManager {
     private static defaultSettings: Settings = {
@@ -19,71 +19,80 @@ export default class SettingsManager {
         homeToggleEnabled: false,
         editorFontSize: 14,
         injectionDelay: 3,
-        autoUpdate: true
-    }
+        autoUpdate: true,
+    };
     public static currentSettings: Writable<Settings> = writable(this.defaultSettings);
     public static settingsDetails = {
         autoInject: {
             name: "Auto Inject",
             description: "Automatically injects into roblox",
-            type: "boolean"
+            type: "boolean",
         },
         injectionDelay: {
             name: "Auto Inject Delay",
             description: "Delay in seconds to prevent automatic injection from crashing",
-            type: "number"
+            type: "number",
         },
         topMost: {
             name: "Top Most",
             description: "Makes KrampUI stay on top of all windows",
-            type: "boolean"
+            type: "boolean",
         },
         homeToggleEnabled: {
             name: "Home Toggle Enabled",
             description: "When enabled, you can press Home on your keyboard to hide/show KrampUI",
-            type: "boolean"
+            type: "boolean",
         },
         editorFontSize: {
             name: "Font Size",
             description: "Controls the editors font size",
-            type: "number"
+            type: "number",
         },
         autoUpdate: {
             name: "Auto Update",
             description: "Automatically updates to latest version on startup",
-            type: "boolean"
-        }
-    }
+            type: "boolean",
+        },
+    };
 
     public static async saveSettings() {
         const settings = get(this.currentSettings);
-        const results = await FileSystemService.writeFile("settings/settings.json", JSON.stringify(settings, null, 2));
+        const results = await FileSystemService.writeFile(
+            "settings/settings.json",
+            JSON.stringify(settings, null, 2)
+        );
         if (!results.success) {
             WindowManager.showFatalErrorPopup(`Failed to save settings. Error: ${results.error}`);
         }
     }
 
-    public static async setSetting(setting: keyof Settings, value: any) {
-        this.currentSettings.update(settings => {
-            return { ...settings, [setting]: value }
-        })
+    public static async setSetting<K extends keyof Settings>(setting: K, value: Settings[K]) {
+        this.currentSettings.update((settings) => {
+            return { ...settings, [setting]: value };
+        });
         this.saveSettings();
     }
 
     public static async initialize() {
-        if (!await FileSystemService.exists("settings/settings.json")) {
-            const results = await FileSystemService.writeFile("settings/settings.json", JSON.stringify(this.defaultSettings, null, 2));
+        if (!(await FileSystemService.exists("settings/settings.json"))) {
+            const results = await FileSystemService.writeFile(
+                "settings/settings.json",
+                JSON.stringify(this.defaultSettings, null, 2)
+            );
             if (!results.success) {
-                WindowManager.showFatalErrorPopup(`Failed to create default settings file. Error: ${results.error}`);
+                WindowManager.showFatalErrorPopup(
+                    `Failed to create default settings file. Error: ${results.error}`
+                );
             }
         } else {
             const settings = await FileSystemService.readFile("settings/settings.json");
             if (settings) {
                 this.currentSettings.set(JSON.parse(settings) as Settings);
             } else {
-                WindowManager.showFatalErrorPopup("Failed to read settings file. Using default settings.");
+                WindowManager.showFatalErrorPopup(
+                    "Failed to read settings file. Using default settings."
+                );
             }
         }
     }
-
 }
